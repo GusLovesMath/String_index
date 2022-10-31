1c2464cb82709610c5bc18406441edcda0ac3101
Gus <109978367+GusLovesMath@users.noreply.github.com>
2022-10-31 16:20:34 -0400

    Create string_index.py

---
+++ string_index.py
@@ -0,0 +1,17 @@
+def string_index(string: str, character: str, case=False) -> dict:
+    """
+    Function that takes a string and a given character and returns
+    the number of times a character appears  in a string and it's indices.
+    """
+    
+    if not case:
+        string = string.lower()
+        
+    count = string.count(character)
+    indices = [i for i, l in enumerate(string) if l == character]
+            
+    string_positions = {'Character': character,
+                        'Indices': indices,
+                        'Count': count}
+    
+    return string_positions
